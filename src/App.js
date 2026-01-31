5c89e2e38e24c1bce84b38f08b161df24d8954a0
David Y. Stephenson <david@davidystephenson.com>
2026-01-31 11:56:13 -0500

    Use reducer for state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import GuestList from './GuestList'
 import Settings from './Settings'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
@@ -6,30 +6,52 @@ import Home from './Home'
 import Indicator from './Indicator'
 import { Context } from './context'
 
+const INVITE = 'INVITE'
+const START = 'START'
+const UNINVITE = 'UNINVITE'
+const FINISH = 'FINISH'
+
 export default function App() {
-  const [guests, setGuests] = useState(['Dorothy', 'Zelda', 'Tallulah'])
-  const [image, setImage] = useState()
-  const [loading, setLoading] = useState(false)
+  function reducer (state, action) {
+    if (action.type === START) {
+      const newState = { ...state, loading: true }
+      return newState
+    }
+    if (action.type === START) {
+      return { ...state, loading: false, image: action.payload }
+    }
+    if (action.type === INVITE) {
+      const newGuests = [...state.guests, action.payload]
+      return { ...state, guests: newGuests }
+    }
+    if (action.type === UNINVITE) {
+      const newGuests = state.guests.filter(guest => guest !== action.payload)
+      return { ...state, guests: newGuests }
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, {
+    guests: ['Dorothy', 'Zelda', 'Tallulah'],
+    image: undefined,
+    loading: false
+  })
+  console.log('state', state)
   async function download() {
-    setLoading(true)
+    dispatch({ type: START })
     const url = 'https://dog.ceo/api/breeds/image/random'
     const response = await fetch(url)
     const data = await response.json()
-    setImage(data.message)
-    setLoading(false)
+    dispatch({ type: FINISH, payload: data.message })
   }
   useEffect(() => {
     download()
   }, [])
   function invite(name) {
-    const newGuests = [...guests, name]
-    setGuests(newGuests)
+    dispatch({ type: INVITE, payload: name })
   }
   function uninvite(name) {
-    const newGuests = guests.filter(guest => guest !== name)
-    setGuests(newGuests)
+    dispatch({ type: UNINVITE, payload: name })
   }
-  const value = { guests, image, loading, download, invite, uninvite }
+  const value = { ...state, download, invite, uninvite }
   return (
     <BrowserRouter>
       <Context value={value}>
